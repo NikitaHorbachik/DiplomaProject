79a1036ef1afdb2bf071e671b1576cd23c3ca764
NikitaHarbachyk <Nikitagor219@gmail.com>
2025-03-07 12:32:36 +0300

    feat(project): initial commit

---
+++ project files/diplomaBackEnd/src/main/java/org/nharbachyk/diplomabackend/DiplomaBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.nharbachyk.diplomabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaBackEndApplication.class, args);
+    }
+
+}
---
+++ project files/diplomaBackEnd/src/test/java/org/nharbachyk/diplomabackend/DiplomaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nharbachyk.diplomabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
